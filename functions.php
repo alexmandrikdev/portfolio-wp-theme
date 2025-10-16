0b0453ad884948e8ee86736973990da800a00d12
Alex András Mándrik <36386967+alexmandrikdev@users.noreply.github.com>
2025-10-16 16:28:41 +0200

    chore: add PHP code quality tools and apply WordPress coding standards (#5)
    - Add Composer configuration with WordPress Coding Standards dependency
    - Add PHP_CodeSniffer configuration for WordPress standards
    - Update PHP files to comply with WordPress coding conventions
    - Add vendor directory to gitignore for Composer dependencies
    - Replace `include` with `require` for theme initialization files
    - Add proper asset file handling for enqueued stylesheets
    - Add CS ignore comments for development functions and commented code

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-include get_parent_theme_file_path('inc/helpers.php');
-include get_parent_theme_file_path('inc/class-theme-init.php');
+require get_parent_theme_file_path( 'inc/helpers.php' );
+require get_parent_theme_file_path( 'inc/class-theme-init.php' );
 
 AMPortfolioTheme\Theme_Init::init();
--- inc/helpers.php
+++ inc/helpers.php
@@ -1,22 +1,21 @@
 <?php
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-if (!function_exists('dump')) {
-    function dump(...$vars)
-    {
-        echo '<pre>';
-        foreach ($vars as $var) {
-            var_dump($var);
-        }
-        echo '</pre>';
-    }
+if ( ! function_exists( 'dump' ) ) {
+	function dump( ...$vars ) {
+		echo '<pre>';
+		foreach ( $vars as $var ) {
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
+			var_dump( $var );
+		}
+		echo '</pre>';
+	}
 }
 
-if (!function_exists('dd')) {
-    function dd(...$vars)
-    {
-        dump(...$vars);
-        die();
-    }
+if ( ! function_exists( 'dd' ) ) {
+	function dd( ...$vars ) {
+		dump( ...$vars );
+		die();
+	}
 }
