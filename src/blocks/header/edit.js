c96191683840a58a568aebc441408cd5ac9d2753
Alex Andras Mandrik <alexmandrik@pm.me>
2025-10-20 12:02:40 +0200

    style: format JavaScript files to follow WordPress coding standards
    Update code formatting to align with WordPress JavaScript standards, including consistent single quote usage, proper spacing in function calls and object literals, and improved code organization. Add unique keys to mapped elements and ensure proper accessibility with ID attributes for form controls.

--- src/blocks/header/edit.js
+++ src/blocks/header/edit.js
@@ -1,6 +1,6 @@
-import { __ } from "@wordpress/i18n";
-import { trash as trashIcon } from "@wordpress/icons";
-import { useBlockProps } from "@wordpress/block-editor";
+import { __ } from '@wordpress/i18n';
+import { trash as trashIcon } from '@wordpress/icons';
+import { useBlockProps } from '@wordpress/block-editor';
 import {
 	BaseControl,
 	Button,
@@ -11,82 +11,93 @@ import {
 	Flex,
 	FlexBlock,
 	FlexItem,
-} from "@wordpress/components";
-import { useSelect } from "@wordpress/data";
-import { store as coreDataStore } from "@wordpress/core-data";
-import "./editor.scss";
-import { useEffect, useState } from "@wordpress/element";
+} from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { store as coreDataStore } from '@wordpress/core-data';
+import './editor.scss';
 
-export default function Edit({ attributes, setAttributes }) {
+export default function Edit( { attributes, setAttributes } ) {
 	const { menu_items: menuItems = [] } = attributes;
 
 	const pages =
-		useSelect((select) => {
-			return select(coreDataStore).getEntityRecords("postType", "page", {
-				per_page: -1,
-			});
-		}) || [];
+		useSelect( ( select ) => {
+			return select( coreDataStore ).getEntityRecords(
+				'postType',
+				'page',
+				{
+					per_page: -1,
+				}
+			);
+		} ) || [];
 
-	const menuItemOptions = pages.map((page) => {
+	const menuItemOptions = pages.map( ( page ) => {
 		return {
 			label: page.title.rendered,
 			value: page.id,
 		};
-	});
+	} );
 
-	const addMenuItem = () => setAttributes({ menu_items: [...menuItems, ""] });
+	const addMenuItem = () =>
+		setAttributes( { menu_items: [ ...menuItems, '' ] } );
 
-	const removeMenuItem = (index) => {
-		const newMenuItems = [...menuItems];
-		newMenuItems.splice(index, 1);
-		setAttributes({ menu_items: newMenuItems });
+	const removeMenuItem = ( index ) => {
+		const newMenuItems = [ ...menuItems ];
+		newMenuItems.splice( index, 1 );
+		setAttributes( { menu_items: newMenuItems } );
 	};
 
-	const updateMenuItem = (index, value) => {
-		const newMenuItems = [...menuItems];
-		newMenuItems[index] = value;
-		setAttributes({ menu_items: newMenuItems });
+	const updateMenuItem = ( index, value ) => {
+		const newMenuItems = [ ...menuItems ];
+		newMenuItems[ index ] = value;
+		setAttributes( { menu_items: newMenuItems } );
 	};
 
 	return (
 		<div
-			{...useBlockProps({
-				style: { paddingLeft: "24px", paddingRight: "24px" },
-			})}
+			{ ...useBlockProps( {
+				style: {
+					paddingLeft: '24px',
+					paddingRight: '24px',
+					marginBottom: '24px',
+				},
+			} ) }
 		>
-			<Card style={{ width: "100%" }}>
+			<Card style={ { width: '100%' } }>
 				<CardHeader>
-					<h4>{__("Header", "portfolio")}</h4>
+					<h4>{ __( 'Header', 'portfolio' ) }</h4>
 				</CardHeader>
 				<CardBody>
 					<BaseControl
 						__nextHasNoMarginBottom
-						label={__("Menu Items", "portfolio")}
+						label={ __( 'Menu Items', 'portfolio' ) }
+						id="header-menu-items"
 					>
-						{menuItems.map((item, index) => (
-							<Flex align="center">
+						{ menuItems.map( ( item, index ) => (
+							<Flex align="center" key={ index }>
 								<FlexBlock>
 									<ComboboxControl
-										key={index}
-										value={item}
-										onChange={(value) => {
-											updateMenuItem(index, value);
-										}}
-										options={menuItemOptions}
+										key={ index }
+										value={ item }
+										onChange={ ( value ) => {
+											updateMenuItem( index, value );
+										} }
+										options={ menuItemOptions }
 									/>
 								</FlexBlock>
 								<FlexItem>
 									<Button
 										size="small"
 										isDestructive
-										icon={trashIcon}
-										onClick={() => removeMenuItem(index)}
+										icon={ trashIcon }
+										onClick={ () =>
+											removeMenuItem( index )
+										}
 									></Button>
 								</FlexItem>
 							</Flex>
-						))}
+						) ) }
 
-						<Button variant="primary" onClick={addMenuItem}>
+						<Button variant="primary" onClick={ addMenuItem }>
 							Add menu item
 						</Button>
 					</BaseControl>
