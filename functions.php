7d0342afb68b89f1fe471bc1d6fbda40a3c775b3
Alex Andras Mandrik <alexmandrik@pm.me>
2025-10-22 16:32:19 +0200

    feat: add project post type and taxonomies
    Introduce a new 'project' custom post type with support for title, editor, thumbnail, and excerpt. Add two associated taxonomies: 'project_technology' and 'project_type' for organizing projects. Include WPML configuration for multilingual support.

--- functions.php
+++ functions.php
@@ -3,4 +3,10 @@
 require get_parent_theme_file_path( 'inc/helpers.php' );
 require get_parent_theme_file_path( 'inc/class-theme-init.php' );
 
+require get_parent_theme_file_path( 'inc/post-types/class-project-cpt.php' );
+require get_parent_theme_file_path( 'inc/taxonomies/class-project-taxonomies.php' );
+
 AMPortfolioTheme\Theme_Init::init();
+
+AMPortfolioTheme\PostTypes\Project_CPT::init();
+AMPortfolioTheme\Taxonomies\Project_Taxonomies::init();
